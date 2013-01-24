4bd156126abfee544c279754de4acffc142a6962
Nick Desaulniers <ndesaulniers@mozilla.com>
2013-01-24 14:29:59 -0800

    generate a manifest

--- generate.rb
+++ generate.rb
@@ -18,11 +18,11 @@ end
 # Get info from user
 puts 'Please provide the following information about your packaged app stub'
 print 'App name: '
-app_name = gets
+app_name = gets().rstrip
 print '128x128px Icon path: '
-icon_path = gets
+icon_path = gets().rstrip
 print 'Future location of package.manifest: '
-future_manifest_location = gets
+future_manifest_location = gets.rstrip()
 puts "\nI will now create a package app stub for"
 puts app_name
 puts 'using the icon from'
@@ -61,29 +61,112 @@ Dir.mkdir './tmp'
 
 # create index.html
 puts 'generating tmp/index.html'
-File.open 'tmp/install.html', 'w' do |f|
+File.open 'tmp/index.html', 'w' do |f|
   f << <<-HEREDOC
+<!DOCTYPE html>
 <html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="pragma" content="no-cache">
+    <title>Packaged Stub Test</title>
+    <style></style>
+  </head>
   <body>
-    <p>Packaged app installation page</p>
-    <script>
-      var manifestUrl = '/package.manifest';
-      var req = navigator.mozApps.installPackage(manifestUrl);
-      req.onsuccess = function() {
-        alert(this.result.origin);
-      };
-      req.onerror = function() {
-        alert(this.error.name);
-      };
+    <h1>Packaged Stub Test 0.1</h1>
+    <button id="updateButton">Update</button>
+    <div id="console"></div>
+    <script type="application/javascript;version=1.8">
+      document.getElementById("updateButton").addEventListener("click",
+        function() update(), false);
+
+      function notify(message) {
+        var console = document.getElementById("console");
+        console.appendChild(document.createTextNode(message));
+        console.appendChild(document.createElement("br"));
+      }
+
+      function update() {
+        var getSelf = navigator.mozApps.getSelf();
+
+        getSelf.onsuccess = function() {
+          var self = getSelf.result;
+
+          notify("Checking for update…");
+          var checkUpdate = self.checkForUpdate();
+
+          checkUpdate.onsuccess = function() {
+            if (self.downloadAvailable) {
+              notify("Update available; downloading and installing update…");
+
+              self.ondownloadsuccess = function onDownloadSuccess(event) {
+                notify("Download success; closing app to complete update…");
+                window.setTimeout(function() window.close(), 3000);
+              };
+
+              self.ondownloaderror = function onDownloadError() {
+                notify("Download error: " + self.downloadError.name);
+              };
+
+              self.ondownloadapplied = function onDownloadApplied() {
+                notify("Download applied too soon; I should have quit by now!");
+              };
+
+              self.download();
+
+            } else {
+              notify("No update available.");
+            }
+          };
+
+          checkUpdate.onerror = function() {
+            notify("Checking for update error: " + checkUpdate.error.name);
+          }
+        };
+
+        getSelf.onerror = function() {
+          notify("Get self error: " + getSelf.error.name);
+        }
+      }
     </script>
   </body>
 </html>
   HEREDOC
 end
 
+# Create app manifest
+File.open 'tmp/manifest.webapp', 'w' do |f|
+  f << <<-HEREDOC
+{
+  "name": "#{app_name}",
+  "description": "a packaged app for testing stub updates",
+  "launch_path": "/index.html",
+  "version": "0.1",
+  "developer": {
+    "name": "Myk Melez",
+    "url": "https://github.com/mykmelez/packaged-app-stub"
+  },
+  "locales": {
+    "en-US": {
+      "name": "PackStubTest",
+      "description": "a packaged app for testing stub updates",
+      "developer": {
+        "name": "Myk Melez",
+        "url": "https://github.com/mykmelez/packaged-app-stub"
+      }
+    }
+  },
+  "default_locale": "en-US",
+  "icons": {
+    "128": "/style/icons/Blank.png"
+  }
+}
+  HEREDOC
+end
+
+
 # TODO
 # create other files...
 # zip up files...
 
 # clean up ./tmp
-FileUtils.rm_rf './tmp'
+#FileUtils.rm_rf './tmp'
